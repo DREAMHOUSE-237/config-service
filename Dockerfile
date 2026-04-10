1e36f28cfe9c5f5bb6658b1080669f6ba5219c4b
HeilTchambaNana <htchamba124@gmail.com>
2026-04-10 22:14:27 +0100

    Service initialisation

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM FROM openjdk:17-jdk-slim
+
+VOLUME /tmp
+
+COPY target/dreamhouse-config-service-0.0.1-SNAPSHOT.jar app.jar
+
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ src/test/java/com/dreamhouse237/dreamhouse_config_service/DreamhouseConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamhouse237.dreamhouse_config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamhouseConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
